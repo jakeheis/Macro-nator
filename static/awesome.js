46f12fade836db15aeab83c9ca0f9d73c1d4952e
Jake Heiser <jake@quizlet.com>
2013-03-25 14:03:00 -0700

    Made perty

--- static/awesome.js
+++ static/awesome.js
@@ -1,8 +1,5 @@
 $(document).ready(function(){
-	$('textarea').keyup(function(){
-		// $.get('convertify', function(data) {
-			// $('#output').text(data);
-		// });
+	$('textarea, input').keyup(function(){
 		var all = $('#first').val()+"\nint main(void) {\n"+$('#second').val()+"\n} //Macro-Terminator";
 		$.post("convertify", { file: all }, function(data) {
 			$('#output').html(data);
